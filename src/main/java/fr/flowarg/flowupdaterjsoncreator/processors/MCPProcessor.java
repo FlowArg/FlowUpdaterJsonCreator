706b2d87d35a114735874c0a98a2704ddb5fbee6
FlowArg <coutureflorence3@gmail.com>
2020-09-20 14:01:12 +0200

    Fixed bad MCP generated json

--- src/main/java/fr/flowarg/flowupdaterjsoncreator/processors/MCPProcessor.java
+++ src/main/java/fr/flowarg/flowupdaterjsoncreator/processors/MCPProcessor.java
@@ -25,18 +25,18 @@ public class MCPProcessor implements IProcessor
         final boolean clientExist = client.exists();
         final File server = new File(dir, "server.jar");
         final boolean serverExist = server.exists();
-        this.mcp = new MCP(((UrlPanel)Panels.URL_PANEL).getDefaultUrl() + client.getName(), clientExist ? FileUtils.getSHA1(client) : "", clientExist ? (int)FileUtils.getFileSizeBytes(client) : -1,
-                           ((UrlPanel)Panels.URL_PANEL).getDefaultUrl() + server.getName(), serverExist ? FileUtils.getSHA1(server) : "", serverExist ? (int)FileUtils.getFileSizeBytes(server) : -1);
+        this.mcp = new MCP(clientExist ? ((UrlPanel)Panels.URL_PANEL).getDefaultUrl() + client.getName() : "", clientExist ? FileUtils.getSHA1(client) : "", clientExist ? (int)FileUtils.getFileSizeBytes(client) : -1,
+                           serverExist ? ((UrlPanel)Panels.URL_PANEL).getDefaultUrl() + server.getName() : "", serverExist ? FileUtils.getSHA1(server) : "", serverExist ? (int)FileUtils.getFileSizeBytes(server) : -1);
     }
 
     @Override
     public void generate(Object... args)
     {
         final JsonObject object = new JsonObject();
-        object.addProperty("clientDownloadURL", this.mcp.getClientDownloadURL());
+        object.addProperty("clientURL", this.mcp.getClientDownloadURL());
         object.addProperty("clientSha1", this.mcp.getClientSha1());
         object.addProperty("clientSize", this.mcp.getClientSize());
-        object.addProperty("serverDownloadURL", this.mcp.getServerDownloadURL());
+        object.addProperty("serverURL", this.mcp.getServerDownloadURL());
         object.addProperty("serverSha1", this.mcp.getServerSha1());
         object.addProperty("serverSize", this.mcp.getServerSize());
         final Gson gson = new GsonBuilder().setPrettyPrinting().create();
